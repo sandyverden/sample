14599a49cf1defb5407053d271480e73d0dc4934
Rizki <riyadirizki.r@gmail.com>
2018-09-30 19:40:55 +0700

    Sample untuk coba jenkins
    A

---
+++ src/main/java/network/powerhouse/cloud/samplejenkin/Controller.java
@@ -0,0 +1,13 @@
+package network.powerhouse.cloud.samplejenkin;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello 1";
+    }
+}
---
+++ src/main/java/network/powerhouse/cloud/samplejenkin/SampleJenkinApplication.java
@@ -0,0 +1,12 @@
+package network.powerhouse.cloud.samplejenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleJenkinApplication.class, args);
+	}
+}
